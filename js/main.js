893d9fea22bfe84e2d44fc17e422c9d0c7f939b5
Andrei Kamarou <mr.akomarov@gmail.com>
2021-03-23 18:15:02 +0300

    Initial commit

--- js/main.js
+++ js/main.js
@@ -12,10 +12,31 @@ const mySwiper = new Swiper('.swiper-container', {
 
 const buttonCart = document.querySelector('.button-cart');
 const modalCart = document.querySelector('#modal-cart');
+const modalClose = document.querySelector('.modal-close');
 
 const openModal = () => {
-	alert('Hello!');
+	modalCart.classList.add('show');
+}
+const closeModal = () => {
+	modalCart.classList.remove('show');
 }
 
 buttonCart.addEventListener('click', openModal);
+modalClose.addEventListener('click', closeModal);
 
+// Smooth scrolling
+
+{
+const scrollLinks = document.querySelectorAll('a.scroll-link');
+
+for(let i = 0; i < scrollLinks.length; i++) {
+	scrollLinks[i].addEventListener('click', function(e) {
+		e.preventDefault();
+		const linkID = scrollLinks[i].getAttribute('href');
+		document.querySelector(linkID).scrollIntoView({
+			behavior: 'smooth',
+			block: 'start'
+			})
+		});
+	}
+}
